71257352879f7ca0fe7ffc675b631aeb577f3700
Sergey Ovsyannikov <ovssv87@gmail.com>
2025-03-29 18:29:33 +0400

    Created Character

---
+++ Source/LeaveMeAlone/Public/Player/MyPlayerController.h
@@ -0,0 +1,17 @@
+// LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAVEMEALONE_API AMyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
